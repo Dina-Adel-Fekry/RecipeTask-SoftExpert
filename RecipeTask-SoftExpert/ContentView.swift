68d7e3d5af4406c75f6a39ab68e124c72976ec1b
Dina-Adel-Fekry <77211164+Dina-Adel-Fekry@users.noreply.github.com>
2021-08-19 13:12:29 +0200

    Initial Commit

---
+++ RecipeTask-SoftExpert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeTask-SoftExpert
+//
+//  Created by Dina Adel on 8/19/21.
+//  Copyright © 2021 Dina Adel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
